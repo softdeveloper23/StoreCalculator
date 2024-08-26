acfe313212a21e3359be8a4081e25e68ea31366a
softdeveloper23 <brannongarrett77@gmail.com>
2024-08-26 13:12:35 +0200

    Updated version 2

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -9,5 +9,17 @@ public class Main {
         System.out.println("Milk chocolate: $4");
         System.out.println("Doughnut: $2.5");
         System.out.println("Pancake: $3.2");
+        System.out.println();
+
+        double total = 202 + 118 + 2250 + 1680 + 1075 + 80;
+        System.out.println("Earned amount:");
+        System.out.println("Bubblegum: $202");
+        System.out.println("Toffee: $118");
+        System.out.println("Ice cream: $2250");
+        System.out.println("Milk chocolate: $1680");
+        System.out.println("Doughnut: $1075");
+        System.out.println("Pancake: $80");
+        System.out.println();
+        System.out.println("Income: $" + total);
     }
 }
