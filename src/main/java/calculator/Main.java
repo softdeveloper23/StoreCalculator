2ed844a13990f95ec90c09c3c5f053b7c5a0347e
softdeveloper23 <brannongarrett77@gmail.com>
2024-09-01 11:39:29 +0200

    Updated to final version

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -3,32 +3,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        double croissants, coffee, iceCream, milkChocolate, donuts, tea;
         Scanner scanner = new Scanner(System.in);
         // Ask user for their name
         System.out.println("What's your name?");
         String name = scanner.nextLine();
-        System.out.printf("Hello, %s!\n", name);
+        System.out.printf("Hello, %s! How much monthly income did you earn for the following items?\n", name);
 
-        int total = 202 + 118 + 2250 + 1680 + 1075 + 80;
-
-        System.out.println("Earned amount:");
-        System.out.println("Bubblegum: $202");
-        System.out.println("Toffee: $118");
-        System.out.println("Ice cream: $2250");
-        System.out.println("Milk chocolate: $1680");
-        System.out.println("Doughnut: $1075");
-        System.out.println("Pancake: $80");
         System.out.println();
-        System.out.println("Income: $" + total);
+        System.out.println("Please enter the amount earned for croissants:");
+        croissants = scanner.nextDouble();
 
-        System.out.println("Staff expenses:");
-        int staffExpenses = scanner.nextInt();
+        System.out.println("Please enter the amount earned for coffee:");
+        coffee = scanner.nextDouble();
 
-        System.out.println("Other expenses:");
-        int otherExpenses = scanner.nextInt();
+        System.out.println("Please enter the amount earned for ice cream:");
+        iceCream = scanner.nextDouble();
 
-        int netIncome = total - (staffExpenses + otherExpenses);
+        System.out.println("Please enter the amount earned for milk chocolate:");
+        milkChocolate = scanner.nextDouble();
 
-        System.out.printf("Net income: $%d", netIncome);
+        System.out.println("Please enter the amount earned for donuts:");
+        donuts = scanner.nextDouble();
+
+        System.out.println("Please enter the amount earned for tea:");
+        tea = scanner.nextDouble();
+
+        double total = croissants + coffee + iceCream + milkChocolate + donuts + tea;
+
+        System.out.println();
+        System.out.printf("Your monthly income is: $%.2f", total);
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("How much were your staff expenses for the month?");
+        double staffExpenses = scanner.nextDouble();
+
+        System.out.println("How much were your other expenses for the month?");
+        double otherExpenses = scanner.nextDouble();
+
+        double netIncome = total - (staffExpenses + otherExpenses);
+
+        System.out.printf("\nYour net income for the month is: $%.2f", netIncome);
     }
 }
